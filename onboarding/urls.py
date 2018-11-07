db3f4ac2b1d04d6f7c0e01b32032e07bf65f79f0
manuelc <manuelc@eventbrite.com>
2018-11-07 18:28:29 -0300

    Added root redirect to players game removed by mistake :P

--- onboarding/urls.py
+++ onboarding/urls.py
@@ -4,11 +4,12 @@ from django.conf.urls import (
 )
 from django.views.generic.base import RedirectView
 from onboarding.views import (
-	PlayersCreateView,
+    PlayersCreateView,
     PlayersView,
 )
 
 urlpatterns = [
     url(r'players/$', PlayersView.as_view(), name='players'),
     url(r'players/create$', PlayersCreateView.as_view(), name='create_player'),
+    url(r'$', RedirectView.as_view(pattern_name='players'), name='root'),
 ]
